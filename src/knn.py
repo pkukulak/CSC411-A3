9a59e75df5cb5228291d334da839017810b2c01d
Philip Kukulak <philip.kukulak@mail.utoronto.ca>
2015-11-30 16:26:53 -0500

    renamed knn_prune to knn

--- src/knn.py
+++ src/knn.py
@@ -1,28 +1,20 @@
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import cross_validation
+from sklearn.cross_validation import train_test_split
 from itertools import groupby
-from util import load_data
+from util import load_data, prune_individuals
 import numpy as np
 
 if __name__ == "__main__":
-    k_vals = [3, 5, 7, 11, 14, 21]
-    rates = []
+    k_vals = [3, 5, 7, 9, 11, 13, 14, 16, 21]
     data_in, data_targ, data_ids, unlabeled_in, test_in = load_data()
     
     N, M = data_in.shape
+    full_data = np.append(np.append(data_in, data_targ, axis=1), data_ids, axis=1)
+
+    pruned_in, pruned_targ, pruned_ids = prune_individuals(full_data)
 
     for k in k_vals:
         kNN = KNeighborsClassifier(n_neighbors=k)
-        full_data = np.append(np.append(data_in, data_targ, axis=1), data_ids, axis=1)
-        sorted_by_id = full_data[full_data[:, M+1].argsort()]
-        '''
-        groups = np.array([])
-        unknows = None
-        for k, v in groupby(sorted_by_id, lambda x: x[M+1]):
-            if k == -1:
-                unknown = v
-            else:
-                groups = np.append(groups, L)
-            print k, v
-            print groups
-        '''
+        train_in, valid_in, train_targ, valid_targ = train_test_split(pruned_in, pruned_targ, test_size=0.33, stratify=pruned_ids)
+        kNN.fit(train_in, train_targ.flatten())
+        print "k = {} ; classification rate = {}".format(k, kNN.score(valid_in, valid_targ))
