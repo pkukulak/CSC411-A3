f21d7eac278e8cf3861fe79a4236468cfe007fff
Philip Kukulak <philip.kukulak@mail.utoronto.ca>
2015-11-30 16:06:24 -0500

    knn yields ~50% rate. moved pruning to util.py

--- src/knn_prune.py
+++ src/knn_prune.py
@@ -1,25 +1,17 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.cross_validation import train_test_split
 from itertools import groupby
-from util import load_data
+from util import load_data, prune_individuals
 import numpy as np
 
-def prune_individuals(full_data):
-    N, M = full_data.shape
-    all_ids = full_data[:, M-1]
-    counts = np.array([])
-    for i in all_ids:
-        counts = np.append(counts, sum(i == all_ids))
-    return full_data[counts != 1]
-    
 if __name__ == "__main__":
-    k_vals = [3, 5, 7, 11, 14, 21]
+    k_vals = [3, 5, 7, 9, 11, 13, 14, 16, 21]
     data_in, data_targ, data_ids, unlabeled_in, test_in = load_data()
     
     N, M = data_in.shape
     full_data = np.append(np.append(data_in, data_targ, axis=1), data_ids, axis=1)
-    pruned_full_data = prune_individuals(full_data[full_data[:, M+1] != -1])
-       
+    pruned_full_data = prune_individuals(full_data)
+     
     Np, Mp = pruned_full_data.shape
 
     pruned_in = pruned_full_data[:, :M]
@@ -28,6 +20,6 @@ if __name__ == "__main__":
 
     for k in k_vals:
         kNN = KNeighborsClassifier(n_neighbors=k)
-        train_in, valid_in, train_targ, valid_targ = train_test_split(pruned_in, pruned_targ, test_size=0.7, stratify=pruned_ids)
+        train_in, valid_in, train_targ, valid_targ = train_test_split(pruned_in, pruned_targ, test_size=0.33, stratify=pruned_ids)
         kNN.fit(train_in, train_targ.flatten())
         print "k = {} ; prediction = {}".format(k, kNN.score(valid_in, valid_targ))
--- src/util.py
+++ src/util.py
@@ -7,7 +7,6 @@ LABELED = "/Users/FILIP/U of T/CSC411/A3/data/labeled_images.mat"
 UNLABELED = "/Users/FILIP/U of T/CSC411/A3/data/unlabeled_images.mat"
 TEST = "/Users/FILIP/U of T/CSC411/A3/data/public_test_images.mat"
 
-#def load_data(labeled_path, unlabeled_path, test_path):
 def load_data():
     '''
     Load all data and return it as a tuple of arrays.
@@ -36,10 +35,34 @@ def load_data():
 
     x, y, z = train_in.shape
     N, M = z, x * y
-    train_in = train_in.reshape(N, M)
+    train_in = train_in.flatten().reshape(M, N).T
     
     unlabeled_in = loaded_unlabeled['unlabeled_images']
     
     test_in = loaded_test['public_test_images']
     
     return train_in, train_targ, train_ids, unlabeled_in, test_in
+
+def prune_individuals(full_data):
+    '''
+    Replace all unique identifiers with a single identifier value.
+    This is done to enable stratifying based on identifier.
+    input:
+        full_data - an NxM+2 matrix, where each row is a datapoint
+                    of M features. the M+1th column is the label
+                    for that datapoint and the M+2th column is the id.
+    output:
+        pruned_data - an NxM+2 matrix, where the only difference from
+                      the input is that any identifiers that appear
+                      once in the M+2th column are replaced with a common
+                      value, 42069.
+    '''
+    N, M = full_data.shape
+    all_ids = full_data[:, M-1]
+    new_ids = np.array([])
+    for i in xrange(all_ids.size):
+        if np.sum(all_ids == all_ids[i]) == 1:
+            new_ids = np.append(new_ids, 42069)
+        else:
+            new_ids = np.append(new_ids, all_ids[i])
+    return np.append(full_data[:, :M-1], new_ids.reshape(-1, 1), axis=1)
