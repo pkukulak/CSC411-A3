693a7a0fa8e09fbef8a6878e4c458b373f9caccf
shaftoes <root@sams-MBP.lan>
2015-12-08 21:21:50 -0500

    added new file: gabor.py

---
+++ src/gabor.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import ndimage as ndi
+
+from skimage import data
+from skimage.util import img_as_float
+from skimage.filters import gabor_kernel
+from skimage.viewer import ImageViewer
+
+from util import load_data
+
+import cPickle as pickle
+
+
+def compute_gabor(image):
+	images = []
+	for theta in np.arange(0.1, np.pi, np.pi / 8):
+		for lmda in np.arange(0.1, np.pi, np.pi/4): 
+
+			imaginary, real = gabor_filter(image, .5,theta=theta,bandwidth=lmda)
+			images.append(real)
+
+	return images
+
+def compute_feats(image, kernels):
+	feats = []
+	for k, kernel in enumerate(kernels):
+		filtered = ndi.convolve(image, kernel, mode='wrap')
+		feats.append(filtered.flatten())
+	return np.vstack(feats).reshape(1,len(kernels)*image.size)
+
+def gabor_features(data_in):
+	#create gabor kernels
+	kernels = []
+	for theta in range(4):
+		theta = theta / 4.0 * np.pi
+		for sigma in (1, 3):
+			for frequency in (0.05, 0.25):
+				kernel = np.real(gabor_kernel(frequency, theta=theta, sigma_x=sigma, sigma_y=sigma))
+				kernels.append(kernel)
+
+	#iterate over data points, and compute the result of applying each gabor kernel
+	m,n = data_in.shape
+	gabor_features = []
+	for i in range(data_in.shape[0]):
+		gabor_features.append(compute_feats(data_in[i].reshape(32,32), kernels))
+	
+
+	return np.vstack(gabor_features)
+
+
+
+if __name__ == "__main__":
+	
+	data_in, data_targ, data_ids, unlabeled_in, test_in = load_data()
+	gabor_feats = gabor_features(data_in)
+	pickle.dump(gabor_feats, open("gabor.p", "wb"))
+
+	
+	
+
+
+	
+
+
+
+
